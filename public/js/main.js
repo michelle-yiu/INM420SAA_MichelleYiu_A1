af45724c7827411ccb7a3471257dce2ae4508ca3
michelle-yiu <yiumichelle@yahoo.com.hk>
2025-05-21 23:07:24 -0400

    nav bar updated

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-// Toggle nav links on mobile
-let menuToggle = document.getElementById('menuToggle');
-let navbarLinks = document.getElementById('navbarLinks');
-
-menuToggle.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
-});
